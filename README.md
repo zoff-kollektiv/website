77779f7f3c44ee5184b6ee30adf6589c52245358
Gustav Pursche <pursche@posteo.de>
2018-07-10 14:49:21 +0200

    Add gatsby boilerplate

---
+++ README.md
@@ -0,0 +1,13 @@
+# Zoff collective
+
+## Install
+
+`npm install`
+
+## Develop
+
+`npm run develop`
+
+## Build
+
+`npm run build`
---
+++ src/pages/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Page = () => (
+  <header className="header">
+    <h1>Zoff collective</h1>
+  </header>
+)
+
+export default Page
