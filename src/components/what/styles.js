d4bb8a6837371befe415d9c46393fa7e49b33ad7
Gustav Pursche <pursche@posteo.de>
2018-07-14 22:56:13 +0200

    What: decrease font-size on small screens

--- src/components/what/styles.js
+++ src/components/what/styles.js
@@ -13,14 +13,15 @@ export default css`
   .what__paragraph {
     color: ${colors.red};
     font-family: ${fonts.mark.family};
-    font-size: 2.4rem;
+    font-size: 1.8rem;
     line-height: 1.3;
-    margin: 0 1rem;
+    margin: 0 2.5rem;
     width: 100%;
   }
 
   @media ${mq.tabletLandscape} {
     .what__paragraph {
+      font-size: 2.4rem;
       margin-left: 4rem;
       margin-right: 4rem;
       max-width: 1000px;
