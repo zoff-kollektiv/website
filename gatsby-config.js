caf63e69a5e4cabccbacfc1c5207bec5b5878d29
Gustav Pursche <pursche@posteo.de>
2018-07-15 15:00:42 +0200

    Allow parsing of markdown files

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,5 +7,13 @@ module.exports = {
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-react-next',
     'gatsby-plugin-styled-jsx',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'projects',
+        path: `${__dirname}/src/projects/`,
+      },
+    },
+    'gatsby-transformer-remark',
   ],
 };
