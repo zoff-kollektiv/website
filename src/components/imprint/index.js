0a5cad85a6bb2201facf53d9a6dc7d77f544ebfe
Gustav Pursche <pursche@posteo.de>
2019-01-24 22:34:15 +0100

    Imprint: add metadata

--- src/components/imprint/index.js
+++ src/components/imprint/index.js
@@ -3,15 +3,19 @@ import React from "react";
 import styles from "./styles";
 
 const Imprint = ({ email }) => (
-  <address className="imprint">
+  <address className="imprint" itemscope itemtype="http://schema.org/Organization">
     <style jsx>{styles}</style>
 
-    <span className="imprint__line">Zoff</span>
-    <span className="imprint__line">Lausitzer Straße 10</span>
-    <span className="imprint__line">Aufgang D, 1. Stock</span>
-    <span className="imprint__line">10999 Berlin</span>
+    <span className="imprint__line" itemprop="legalName">Zoff</span>
+
+    <div itemprop="address">
+      <span className="imprint__line">Lausitzer Straße 10</span>
+      <span className="imprint__line">Aufgang D, 1. Stock</span>
+      <span className="imprint__line">10999 Berlin</span>
+    </div>
+
     <span className="imprint__line">
-      <a href={`mailto:${email}`} className="imprint__link">
+      <a href={`mailto:${email}`} className="imprint__link" itemprop="email">
         {email}
       </a>
     </span>
